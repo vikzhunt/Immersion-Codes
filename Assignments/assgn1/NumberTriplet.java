731442a31e54811375ca181e6c603681eaf413a5
Vikash Singh <132084890+vikzhunt@users.noreply.github.com>
2024-06-18 16:47:43 +0530

    NumberTriplet.java

--- Assignments/assgn1/NumberTriplet.java
+++ Assignments/assgn1/NumberTriplet.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class NumberTriplet {
     public static void main(String[] args) {
-        Scanner vk = new Scanner(System.in);
-        int tc = vk.nextInt();
-        while(tc-->0){
-            int n=vk.nextInt();
-            if (n<7 || n==9) System.out.println("NO");
-            else {
-                System.out.println("YES");
-                if (n % 3 == 0) System.out.println(1+" "+4+" "+(n-5));
-                else if (n % 3 == 1) System.out.println(1+" "+2 +" "+(n-3));
-                else System.out.println(2+" "+3+" "+(n-5));
-            }
-        }
-        vk.close();
-    }
+		Scanner vk = new Scanner(System.in);
+		int tc = vk.nextInt();
+		while(tc-->0){
+		    int n= vk.nextInt();
+		    find(n);
+		}
+	}
+	public static void find(int n){
+	    if(n<6) System.out.println("NO");
+	    else{
+	        int i=1;
+	        int j=2;
+	        int k=n-i-j;
+	        while(k%3==0 || j%3==0){
+	            j+=1;
+	            k-=1;
+	        }
+	        if(j>=k) System.out.println("NO");
+	        else System.out.println("YES\n"+i+" "+j+" "+k);
+	    }
+	}
 }
